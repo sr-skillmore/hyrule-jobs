2c3442c6b7d5bf50bd5e5934cd25af5d82101435
Sandhya Reddy <Sandhya.Reddy@phoenix.edu>
2023-03-10 17:03:34 -0600

    added an object type Job; composition api with ref of a generic type in setup with ts

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,47 @@
 <template>
   <div class="app">
-    <p>{{ name }} - {{ age }}</p>
-    <button @click="changeName('zelda')">change name</button>
-    <button @click="changeAge(30)">change age</button>
+    <p>{{ jobs[0].location }}</p>
   </div>
 </template>
 
 <script lang="ts">
-// this lesson uses Options API
-import { defineComponent, reactive, ref, toRefs } from "vue";
+// this lesson uses Composition API
+import { defineComponent, ref } from "vue";
+import Job from "./types/Job";
 
 export default defineComponent({
   name: "App",
   components: {},
   setup() {
-    // const state = reactive({
-    //   name: "Link",
-    //   age: 25 as number | string,
-    // });
+    const jobs = ref<Job[]>([
+      {
+        title: "farm worker",
+        location: "lon lon ranch",
+        salary: 30000,
+        id: "1",
+      },
+      {
+        title: "quarryman",
+        location: "death mountain",
+        salary: 40000,
+        id: "2",
+      },
+      {
+        title: "flute player",
+        location: "the lost woods",
+        salary: 35000,
+        id: "3",
+      },
+      { title: "fisherman", location: "lake hylia", salary: 21000, id: "4" },
+      {
+        title: "prison guard",
+        location: "gerudo valley",
+        salary: 32000,
+        id: "5",
+      },
+    ]);
 
-    // // state.name = 999; // cannot change type as detected as string by inference
-    // // state.age = "26"; // can be string or number
-    // // state.age = 30; // can be string or number
-
-    // return { ...toRefs(state) };
-
-    const name = ref("Link");
-    const age = ref<number | string>(25);
-
-    return { name, age };
-  },
-  methods: {
-    changeName(name: string) {
-      this.name = name;
-      return name;
-    },
-    changeAge(age: number | string) {
-      this.age = age;
-      return age;
-    },
+    return { jobs };
   },
 });
 </script>
