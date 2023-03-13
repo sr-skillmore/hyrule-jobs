5578b4dbee2a0275d4d9d99da6d8f63513ef42b8
Sandhya Reddy <Sandhya.Reddy@phoenix.edu>
2023-03-13 09:59:51 -0500

    functions for click events

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app">
-    <JobsList :jobs="jobs" />
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+
+    <JobsList :jobs="jobs" :order="order" />
   </div>
 </template>
 
@@ -9,6 +17,7 @@
 import { defineComponent, ref } from "vue";
 import JobsList from "./components/JobsList.vue";
 import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
 
 export default defineComponent({
   name: "App",
@@ -42,9 +51,31 @@ export default defineComponent({
       },
     ]);
 
-    return { jobs };
+    const order = ref<OrderTerm>("title");
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+
+    return { jobs, handleClick, order };
   },
 });
 </script>
 
-<style></style>
+<style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
